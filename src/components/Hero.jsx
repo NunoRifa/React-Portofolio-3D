882fd85cfcd535215ba799c2ca97c6c5ad129dbe
Nuno Rifa <nuno180202@gmail.com>
2023-03-24 14:57:16 +0700

    Tidy up code and create Meta data

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Nuno</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I Front End Developer Enthusiastic, User{" "}
+            I Front End Developer Enthusiast, User{" "}
             <br className="sm:block hidden"></br> Interfaces and Web
             Applications.
           </p>
